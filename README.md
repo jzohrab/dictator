bd77d2e3e892e79e37cb80beb4ba420ff646ebc5
Jeff Zohrab <jzohrab@gmail.com>
2022-02-13 21:05:22 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+A simple Ruby script to dictate files.
+
+I often create small audio scripts that I want the computer to read aloud ...  But it's a hassle to write code, so this thing takes a file as an argument and then reads it aloud using the built in Mac voices.
+
+See the sample.txt for an example.
+
+Usage:
+
+```
+ruby main.rb sample.txt
+```
---
+++ main.rb
@@ -0,0 +1,70 @@
+# Read the supplied file.
+# Interpret each line as a command and read it.
+
+lines = File.read(ARGV[0]).
+          split("\n").
+          map { |s| s.strip }.
+          map { |s| s.gsub(/#.*$/, '') }.
+          select { |s| s !~ /^$/ }
+          
+# puts lines
+
+$voice = 'Daniel'  # Default
+
+def say(s)
+  `say -v #{$voice} "#{s}"`
+end
+
+def pause(s)
+  puts "pausing #{s.to_f}"
+  sleep(s.to_f)
+end
+
+def count(s)
+  parts = s.split(' ')
+  i = parts[0].to_i
+  while i <= parts[2].to_i
+    say(i)
+    pause(parts[4])
+    i += 1
+  end
+end
+
+def beep(s)
+  parts = s.split(' ')
+  count = parts[0].to_i
+  wait = 1
+  if (parts.length == 4)
+    wait = parts[3]
+  end
+  i = 0
+  while i < count
+    puts i
+    puts `tput bel`
+    pause(wait)
+    i += 1
+  end
+end
+
+lines.each do |lin|
+  puts '-' * 20
+  puts lin
+  parts = lin.split(' ')
+  command = parts.shift
+  args = parts.join(' ')
+  case command
+  when 'SAY'
+    say(args)
+  when 'COUNT'
+    count(args)
+  when 'PAUSE'
+    pause(args)
+  when 'BEEP'
+    beep(args)
+  when 'EXIT'
+    puts 'Quitting ...'
+    exit(0)
+  else
+    puts "Unknown command #{command}"
+  end
+end
