9f2523b058adfbcae6a088ff756de73728c3b79b
Jeff Zohrab <jzohrab@gmail.com>
2022-03-01 14:14:47 -0600

    Extract dictator class.

---
+++ dictator.rb
@@ -0,0 +1,85 @@
+# Dictates each line aloud.
+class Dictator
+  
+  def initialize(istest = false)
+    @voice = 'Daniel'  # Default
+    @istest = istest
+  end
+  
+  def say(s)
+    puts "Saying #{s}"
+    `say -v #{@voice} "#{s}"` unless @istest
+  end
+
+  def pause(s)
+    puts "pausing #{s.to_f}"
+    sleep(s.to_f) unless @istest
+  end
+
+  def count(s)
+    parts = s.split(' ')
+    i = parts[0].to_i
+    wait = 1
+    if (parts.length == 5)
+      wait = parts[4]
+    end
+
+    while i <= parts[2].to_i
+      say(i)
+      pause(wait)
+      i += 1
+    end
+  end
+
+  def waitforuser(s)
+    puts "Hit key to continue"
+    $stdin.flush
+    $stdin.gets()
+    puts "ok ..."
+  end
+
+  def beep(s)
+    parts = s.split(' ')
+    count = parts[0].to_i
+    wait = 1
+    if (parts.length == 4)
+      wait = parts[3]
+    end
+    i = 0
+    while i < count
+      puts i
+      puts `tput bel` unless @istest
+      pause(wait)
+      i += 1
+    end
+  end
+
+  # Main method.
+  def dictate(lines)
+    lines.each do |lin|
+      puts '-' * 20
+      puts lin
+      parts = lin.split(' ')
+      command = parts.shift
+      args = parts.join(' ')
+      case command
+      when 'SAY'
+        say(args)
+      when 'COUNT'
+        count(args)
+      when 'PAUSE'
+        pause(args)
+      when 'BEEP'
+        beep(args)
+      when 'WAITFORUSER'
+        waitforuser(args)
+      when 'EXIT'
+        puts 'Quitting ...'
+        return
+      else
+        puts "Unknown command #{command}"
+      end
+    end
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 # Read the supplied file.
 # Interpret each line as a command and read it.
 
+require_relative './dictator'
+
 lines = File.read(ARGV[0]).
           split("\n").
           map { |s| s.strip }.
@@ -11,77 +13,5 @@ lines = File.read(ARGV[0]).
 
 ISTEST = !ENV['TEST'].nil?
 
-$voice = 'Daniel'  # Default
-
-def say(s)
-  puts "Saying #{s}"
-  `say -v #{$voice} "#{s}"` unless ISTEST
-end
-
-def pause(s)
-  puts "pausing #{s.to_f}"
-  sleep(s.to_f) unless ISTEST
-end
-
-def count(s)
-  parts = s.split(' ')
-  i = parts[0].to_i
-  wait = 1
-  if (parts.length == 5)
-    wait = parts[4]
-  end
-
-  while i <= parts[2].to_i
-    say(i)
-    pause(wait)
-    i += 1
-  end
-end
-
-def waitforuser(s)
-  puts "Hit key to continue"
-  $stdin.flush
-  $stdin.gets()
-  puts "ok ..."
-end
-
-def beep(s)
-  parts = s.split(' ')
-  count = parts[0].to_i
-  wait = 1
-  if (parts.length == 4)
-    wait = parts[3]
-  end
-  i = 0
-  while i < count
-    puts i
-    puts `tput bel` unless ISTEST
-    pause(wait)
-    i += 1
-  end
-end
-
-lines.each do |lin|
-  puts '-' * 20
-  puts lin
-  parts = lin.split(' ')
-  command = parts.shift
-  args = parts.join(' ')
-  case command
-  when 'SAY'
-    say(args)
-  when 'COUNT'
-    count(args)
-  when 'PAUSE'
-    pause(args)
-  when 'BEEP'
-    beep(args)
-  when 'WAITFORUSER'
-    waitforuser(args)
-  when 'EXIT'
-    puts 'Quitting ...'
-    exit(0)
-  else
-    puts "Unknown command #{command}"
-  end
-end
+d = Dictator.new(ISTEST)
+d.dictate(lines)
